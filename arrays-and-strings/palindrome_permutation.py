0d62ea033d3ead61db2f36a27c993f39f724df44
Prashant Singh <prashant_singh@live.in>
2020-03-12 00:11:38 -0500

    arrays and strings: palindrome permutation

--- arrays-and-strings/palindrome_permutation.py
+++ arrays-and-strings/palindrome_permutation.py
@@ -7,14 +7,26 @@
 # Input:                    Output:
 # Tact Coa                  True (permutations - "taco cat", "atco cta")
 
-def palindrome_permutation(str):
+def palindrome_permutation(string):
+    string = "".join(string.split())
+    print(string)
     chars_dict = dict()
-    for char in str:
+    for char in string:
         chars_dict[char] = chars_dict.get(char, 0) + 1
 
+    print(chars_dict)
     odd_counter = 0
     for freq in chars_dict.values():
-        if (freq % 2 == 1) and (odd_counter + 1) > 1:
-            return False
+        print(freq, odd_counter)
+        if freq % 2 == 1:
+            if odd_counter + 1 > 1:
+                return False
+            odd_counter += 1
 
     return True
+
+if __name__ == "__main__":
+    print(palindrome_permutation("taco cat"))               # should print True
+    print(palindrome_permutation("car car car"))            # should print False
+    print(palindrome_permutation("catr car car"))           # should print True
+
