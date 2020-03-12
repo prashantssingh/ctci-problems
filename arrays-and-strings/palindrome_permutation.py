ecd9f30838b4bbeb3da369a506c0948b57d2a4d3
Prashant Singh <prashant_singh@live.in>
2020-03-11 23:49:53 -0500

    arrays and strings: palidrome permutation

---
+++ arrays-and-strings/palindrome_permutation.py
@@ -0,0 +1,9 @@
+# QUESTION:
+# Given a string, write a function to check if it is a permutatin of a palindrome. A palindrome is
+# a word or phrase that is the same forwards and backwards. 
+# You can ignore casing and non-letter characters.
+# 
+# EXAMPLE:
+# Input:                    Output:
+# Tact Coa                  True (permutations - "taco cat", "atco cta")
+
